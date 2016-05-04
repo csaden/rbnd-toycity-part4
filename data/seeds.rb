417fc68cfb38091bebb2fde08be3636d787b4cec
Chris Saden <chris.saden@udacity.com>
2016-05-04 16:00:46 -0700

    clean up file to seed db

--- data/seeds.rb
+++ data/seeds.rb
@@ -6,17 +6,8 @@ require_relative '../lib/product'
 
 def db_seed
   # Use faker gem to generate arrays with fake brands, product names, and prices.
-  names = []
-  10.times do names << Faker::Commerce.product_name end
-
-  brands = []
-  10.times do brands << Faker::Company.name end
-
-  prices = []
-  10.times do prices << Faker::Commerce.price end
-
+  10.times do
   # Create 10 new Product objects, and save them to the database
-  products = (names.zip brands, prices).map do |name, brand, price|
-    Product.create name: name, brand: brand, price: price
+    Product.create name: Faker::Commerce.product_name, brand: Faker::Company.name, price: Faker::Commerce.price
   end
 end
